4c39a4aee168d7a515ee6c40982b4de7f39dfc94
Abhay Kumar Nayak <abhaykunayak@gmail.com>
2022-09-12 10:44:30 -0700

    Update SMUServer.py
    minor changes

--- SMUServer.py
+++ SMUServer.py
@@ -14,6 +14,7 @@ class CurrentPoll(Thread):
     def __init__(self,smu):
         Thread.__init__(self)
         self.daemon = True
+        self.delay = 0.01
         self.smu = smu
         self.I = 0
         self.start()
@@ -22,6 +23,7 @@ class CurrentPoll(Thread):
         while True:
             try:
                 self.I = self.smu.read_i()
+                sleep(self.delay)
             except:
                 print("Current read error.")
 
@@ -37,5 +39,5 @@ if __name__ == "__main__":
     smu_server = CurrentPoll(smu)
     
     while True:
-        print("temp A = {}".format(smu_server.I))
+        print("Current = {:3.3f} nA".format(smu_server.I*1e9))
         sleep(1)
