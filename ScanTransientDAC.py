6f10415e41380b31336357a15e98055d86001410
Abhay Kumar Nayak <abhaykunayak@gmail.com>
2023-07-10 19:20:05 -0700

    data also saved as *.mat

--- ScanTransientDAC.py
+++ ScanTransientDAC.py
@@ -7,6 +7,7 @@ Created on Fri Sep  9 18:50:53 2022
 
 import time
 import numpy as np
+import scipy.io as sio
 import labrad
 import yaml
 from datetime import datetime
@@ -18,18 +19,16 @@ import MirrorScan
 
 class Transient:
     
-    def __init__(self,dv,ds,dac,dac_e,dac_a,smu_e,smu_a,ls350,tempServer):
+    def __init__(self, params, dv, ds, dac, ls350, tempServer):
         self.idx = 0
+        self.rootdir = params['ROOTDIR']
+        self.datadir = params['DATADIR']
+        self.datapath = self.rootdir+"\\"+self.datadir+".dir"
         self.dv = dv
         self.ds = ds
         self.dac = dac
-        self.dac_e = dac_e
-        self.dac_a = dac_a
-        self.smu_e = smu_e
-        self.smu_a = smu_a
         self.ls = ls350
         self.tempServer = tempServer
-        self.SMUServer_e = 0
         self.I_e = 0
         self.I_a = 0
         self.v_gate = 0
@@ -120,20 +119,6 @@ class Transient:
         self.delay_ps = np.linspace((delay_start/0.299792485)*2,
                                     (delay_end/0.299792458)*2,pts)
         
-    def voltage_ramp_smu(self,smu,v_initial,v_final):
-        
-        self.log_message("Starting SMU voltage ramp...")
-        if v_initial == v_final:
-            return
-        step = 10
-        smu.output_on()
-        v_steps = np.linspace(v_initial, v_final, step)
-        for i in range(step):
-            smu.set_volts(v_steps[i],1e-6)
-            time.sleep(0.1)
-        time.sleep(1)
-        self.log_message("Voltage SMU ramp ended.")
-        
     def voltage_ramp_dac(self,dac,ch,v_initial,v_final):
         
         self.log_message("Starting DAC voltage ramp on CH {}...".format(ch))
@@ -165,7 +150,7 @@ class Transient:
         self.ds.gpib_write("1PA{:.6f}".format(params['DELAY_RANGE_MM'][0]))
         self.ds.gpib_write("1WS1000")
         
-        self.wait_sec(30)
+        self.wait_sec(15)
         
         self.log_message("Moving stage timeout.")        
         
@@ -177,46 +162,13 @@ class Transient:
         data = np.array([dmm,dps,in1,in2,in3,in4,self.tempD4,self.tempD5,
                          self.I_e,self.I_a,delay_pos])
         self.dv.add(data)
-        
-    def scan_mirror(self,params,k=1):
-        # Ramp bias voltage
-        self.voltage_ramp_smu(self.smu_e, self.smu_a.read_v(), params['BIAS_E'])
-        self.voltage_ramp_smu(self.smu_a, self.smu_a.read_v(), params['BIAS_E'])
-        
-        # Instatiate mirror scan object
-        scan_e = MirrorScan.Scan('E',self.smu_e,self.dac_e,0,1,False)
-        scan_a = MirrorScan.Scan('A',self.smu_a,self.dac_a,2,3,False)
-        
-        # Scan Ramge
-        scan_e.set_scan_range(self.e_max_x,self.e_max_y, self.range/k,self.step/k)
-        scan_a.set_scan_range(self.a_max_x,self.a_max_y, self.range/k,self.step/k)
-        
-        # Scan the Mirror
-        scan_e.start()
-        scan_a.start()
-        
-        # Wait to finish scan
-        scan_e.join()
-        scan_a.join()
-        
-        # Ramp down bias on A
-        self.voltage_ramp_smu(self.smu_a, self.smu_a.read_v(), 0)
-        
-        # Save max data
-        self.e_max_I = scan_e.max_I
-        self.I_e = self.e_max_I
-        self.e_max_x = scan_e.max_x
-        self.e_max_y = scan_e.max_y
-        
-        self.a_max_I = scan_a.max_I
-        self.I_a = self.a_max_I
-        self.a_max_x = scan_a.max_x
-        self.a_max_y = scan_a.max_y
     
+    def save_to_mat(self,data):
+            print('[{}] Saving to *.mat...'.format(self.current_time()))
+            sio.savemat(self.datapath+"\\"+self.dv.get_name()+".mat",
+                        {'data':data})
+                
     def scan_transient(self,params):
-        
-        self.SMUServer_e = SMUServer.CurrentPoll(self.smu_e)
-        # self.SMUServer_a = SMUServer.CurrentPoll(self.smu_a)
         
         for i in range(params['DELAY_POINTS']):
                 percent = (self.delay_mm[i]-np.min(self.delay_mm))/(np.max(self.delay_mm)-np.min(self.delay_mm))
@@ -232,9 +184,6 @@ class Transient:
                     # delay_pos = float(self.ds.gpib_query("1TP?"))
                 except:
                     self.log_message("Stage movement error.")
-                
-                self.I_e = self.SMUServer_e.I
-                # self.I_a = self.SMUServer_a.I    
                 
                 # Buffer ramp DAC
                 br_data = np.array(self.dac.buffer_ramp(params['DAC_OUTPUT_CH_DUMMY'],
@@ -252,9 +201,6 @@ class Transient:
                 
                 self.save_to_datavault(self.delay_mm[i], self.delay_ps[i], 
                                        in1, in2, in3, in4, delay_pos=0)
-        
-        self.SMUServer_e.stop_thread = True
-        # SMUServer_a.stop_thread = True
     
     def scan_transient_fast(self,params):
         
@@ -289,6 +235,7 @@ class Transient:
                                   np.ones((1,params['FPOINTS']))*self.I_a),axis=0).T
         
         self.dv.add(dv_data)
+        self.save_to_mat(dv_data)
             
         # Start moving stage
         self.log_message("Moving to start position...")
@@ -339,52 +286,7 @@ class Transient:
                     self.scan_transient(params)
                 
         except KeyboardInterrupt:
-            self.log_message("Sweep measurement interrupted.")
-        
-    def scan_transient_temp_sweep(self,params):
-        temp_range = np.linspace(params['T_INITIAL'],params['T_FINAL'],params['T_STEPS'])
-        try:
-            for t in range(params['T_STEPS']):
-                self.log_message('Starting temperature sweep #: {} of {}'.format(t,params['T_STEPS']))
-                self.tempD4 = self.tempServer.tempD4
-                self.tempD5 = self.tempServer.tempD5
-                
-                # Set temperature
-                self.log_message("Setting temperature T = {:.2f} K...".format(temp_range[t]))
-                self.ls.set_p(1,temp_range[t])
-                #self.log_message("Setting output #2 set point: {:.3f}.".format(temp_range[t]-3.0))
-                #self.ls.set_p(2,temp_range[t]-3.0)
-
-                time.sleep(60*2)
-                
-                # Rough Scans
-                self.log_message('Mid mirror Scan E and A...')
-                self.scan_mirror(params,k=5)
-                
-                # Transient
-                self.scan_transient_sweep(params)
-                
-        except KeyboardInterrupt:
-            self.log_message("Temp sweep measurement interrupted.")
-
-    def scan_transient_gate_sweep(self,params):
-        gate_range = np.linspace(params['V_GATE_I'],params['V_GATE_F'],params['V_GATE_STEPS'])
-        try:
-            for v in range(params['V_GATE_STEPS']):
-                self.log_message('Starting Gate sweep #: {} of {}'.format(v,gate_range[v]))
-                
-                # Set Gate Voltage
-                self.log_message("Setting Gate Voltage V = {:.2f} V...".format(gate_range[v]))
-                self.v_gate = gate_range[v]
-                self.voltage_ramp_dac(self.dac,params['DAC_OUTPUT_CH'],
-                                      self.dac.read_voltage(2),
-                                      gate_range[v])
-                
-                # Transient
-                self.scan_transient_sweep(params)
-                
-        except KeyboardInterrupt:
-            self.log_message("Gate sweep measurement interrupted.")                
+            self.log_message("Sweep measurement interrupted.")           
             
 def main():
     
@@ -412,25 +314,6 @@ def main():
     dac.initialize()
     for i in range(4):
         dac.set_conversiontime(i,params['DAC_CONV_TIME'])
-        
-    # K2400
-    smu2400 = cxn.k2400()
-    smu2400.select_device()
-    smu2400.gpib_write(':SENSe:CURRent:NPLCycles {}'.format(params['NPLC']))
-    smu2400.gpib_write(":ROUTe:TERMinals FRONt")
-    
-    # smu2400_current = SMUServer.CurrentPoll(smu2400)
-    
-    # K2450
-    smu2450 = cxn.k2450()
-    smu2450.select_device()
-    smu2450.gpib_write(':SENSe:CURRent:NPLCycles {}'.format(params['NPLC']))
-    smu2450.gpib_write(':SENSe:AVERage:TCONtrol {}'.format(params['FILT_TYPE']))
-    smu2450.gpib_write(':SENSe:AVERage:COUNt {}'.format(params['FILT_COUNT']))
-    smu2450.gpib_write(':SENSe:AVERage {}'.format(params['FILT']))
-    smu2450.set_i_read_range(params['SMU_RANGE'])
-    
-    # smu2450_current = SMUServer.CurrentPoll(smu2450)
     
     # LakeShore 350
     ls350 = cxn.lakeshore_350()
@@ -455,8 +338,12 @@ def main():
     dv.cd(params['DATADIR']) # Change to data directory
     
     # Instantiate scan transient object
-    scanTransient = Transient(dv, ds, dac, dac_m, dac_m, smu2400, smu2450, 
-                              ls350, tempServer)
+    scanTransient = Transient(params,
+                              dv, 
+                              ds, 
+                              dac, 
+                              ls350, 
+                              tempServer)
     
     # Set up delay
     if params['MEASURE_MODE'] == 'FAST':
@@ -478,8 +365,7 @@ def main():
     scanTransient.log_message('Measurement Started')
     
     # Voltage ramp up on E SMU
-    scanTransient.voltage_ramp_smu(smu2400, smu2400.read_v(), params['BIAS_E'])
-    scanTransient.voltage_ramp_dac(dac,0,0,10)
+    scanTransient.voltage_ramp_dac(dac,params['DAC_OUTPUT_CH'],0,params['BIAS_E'])
     try:
         # Rough Scans
         #scanTransient.log_message('Coarse mirror Scan E and A...')
@@ -490,22 +376,9 @@ def main():
         
         # Sweeps
         scanTransient.scan_transient_sweep(params)
-        
-        # Temperature sweeps - comment below to remove variable temperature scans
-        #scanTransient.scan_transient_temp_sweep(params)
-        
-        # Gate Sweeps
-        #scanTransient.scan_transient_gate_sweep(params)
             
         # Kill Temperature server
         tempServer.stop_thread = True
-        
-        try:
-            # Kill SMU server
-            scanTransient.SMUServer_e.stop_thread = True
-            scanTransient.SMUServer_a.stop_thread = True
-        except:
-            scanTransient.log_message("SMU servers are dead already!.")
             
     except:
         scanTransient.log_message("Safe exit in process.")
@@ -514,11 +387,9 @@ def main():
     scanTransient.init_stage_position(params)
     
     # Gate Voltage ramp down
-    scanTransient.voltage_ramp_dac(dac,params['DAC_OUTPUT_CH'],dac.read_voltage(2),0)
+    scanTransient.voltage_ramp_dac(dac,params['DAC_OUTPUT_CH'],params['BIAS_E'],0)
     
     # Voltage ramp down
-    scanTransient.voltage_ramp_smu(smu2400, smu2400.read_v(), 0)
-    scanTransient.voltage_ramp_smu(smu2450, smu2450.read_v(), 0)
     scanTransient.voltage_ramp_dac(dac,0,10,0)
     
     # Measurements end
